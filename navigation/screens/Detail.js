656de690dbb5d8575fe0836bf4b33a827a0b72bc
jungwooparkk <148513400+jungwooparkk@users.noreply.github.com>
2023-11-30 03:54:46 +0900

    v0.0.61

--- navigation/screens/Detail.js
+++ navigation/screens/Detail.js
@@ -1,53 +1,217 @@
-// screens/DetailScreen.js
-import React from 'react';
-import { View, Text, Image, StyleSheet,Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, StyleSheet, TextInput, Alert, FlatList, ScrollView } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
+import { Button, Text, Card } from "@rneui/themed";
+import { Rating } from "react-native-ratings";
 
-const Detail  = ({ navigation }) => {
-  const [itemDetail, setItemDetail] = useState({
+const Detail = ({ route, navigation }) => {
+  const itemDetail = {
     id: 1,
-    name: '',
     image: require('../../images/alcholicons/sd.jpg'),
-    price: 0,
-    description: '',
-  });
+    name: '가상의 술',
+    price: 10000,
+    description: '가상의 술에 대한 설명이 들어갑니다.',
+  };
 
-  useEffect(() => {
-    // 가상의 데이터를 가져와서 상태 변수에 설정합니다.
-    const fetchData = async () => {
-      // 실제 데이터베이스에서 데이터를 가져온다고 가정하고, 해당 데이터를 설정합니다.
-      const fetchedData = {
-        id: 1,
-        name: '가상의 술',
-        image: require('../../images/alcholicons/sd.jpg'),
-        price: 10000,
-        description: '가상의 술에 대한 설명이 들어갑니다.',
-      };
-      setItemDetail(fetchedData);
+  // 리뷰 목록과 리뷰 작성 상태
+  const [reviews, setReviews] = useState([]);
+  const [ratingValue, setRatingValue] = useState(3);
+  const [review, setReview] = useState('');
+  const [visibleReviews, setVisibleReviews] = useState([]);
+  const [visibleReviewCount, setVisibleReviewCount] = useState(2); // Initial number of visible reviews
+  const [showLoadMore, setShowLoadMore] = useState(true);
+
+  // 리뷰 평균 별점 계산 함수
+  const calculateAverageRating = () => {
+    if (reviews.length === 0) {
+      return 0; // 리뷰가 없는 경우 0 반환
+    }
+
+    const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+    return totalRating / reviews.length;
+  };
+
+  // 리뷰 제출 함수
+  const handleReviewSubmit = () => {
+    // 리뷰 목록 업데이트
+    const newReview = {
+      id: reviews.length + 1,
+      rating: ratingValue,
+      comment: review,
     };
-      fetchData();
-    }, []); 
+
+    setReviews([...reviews, newReview]);
+
+    // 리뷰 작성 후 필요한 로직을 추가하세요.
+    console.log('Submitted Review:', newReview);
+
+    // 평균 별점 업데이트
+    const averageRating = calculateAverageRating().toFixed(1);
+    Alert.alert('리뷰가 제출되었습니다.', `평균 별점: ${averageRating}`, [
+      { text: '확인' },
+    ]);
+  };
+  // Like function
+  const handleLike = (reviewId) => {
+    // Find the review by ID
+    const updatedReviews = reviews.map((review) =>
+      review.id === reviewId
+        ? { ...review, liked: !review.liked }
+        : review
+    );
+  
+    // Update the reviews state
+    setReviews(updatedReviews);
+  };
   
 
-  return (
-    
-    <View style={styles.container}>
+  // Load more reviews function
+  const loadMoreReviews = () => {
+    const nextVisibleReviewCount = visibleReviewCount + 2; // Increase the count by 2
+    setVisibleReviewCount(nextVisibleReviewCount);
 
-      <Text style={styles.title}>술 상세 정보</Text>
-      <Image source={itemDetail.image} style={styles.image} />
-      <Text style={styles.detailText}>
-        <Text style={styles.labelText}>술 이름:</Text> {itemDetail.name}
-      </Text>
-      <Text style={styles.detailText}>
-{itemDetail.price}
-      </Text>
-      <Text style={styles.detailText}>
-        <Text style={styles.labelText}>설명:</Text> {itemDetail.description}
-      </Text>
-      <Button
-        title="테이스팅노트 작성"
-        onPress={() => navigation.navigate('WriteDownNote')}
-      />
-    </View>
+    // Update the visible reviews based on the new count
+    const nextVisibleReviews = reviews.slice(0, nextVisibleReviewCount);
+    setVisibleReviews(nextVisibleReviews);
+
+    // Toggle the "Load More" button based on whether more reviews are available
+    setShowLoadMore(nextVisibleReviewCount < reviews.length);
+  };
+
+  // Close reviews function
+  const closeReviews = () => {
+    setVisibleReviewCount(2); // Reset to the initial count
+    const firstVisibleReviews = reviews.slice(0, visibleReviewCount);
+    setVisibleReviews(firstVisibleReviews);
+
+    // Toggle the "Load More" button
+    setShowLoadMore(true);
+
+    
+  };
+
+
+  useEffect(() => {
+    // 리뷰 목록을 가져오는 비동기 함수 (예: 서버에서 데이터를 가져오는 로직)
+    const fetchReviews = async () => {
+      try {
+        // 실제로는 여기에 API 호출이나 데이터베이스에서 리뷰를 가져오는 로직을 추가해야 합니다.
+        // 임시로 몇 개의 더미 리뷰를 추가하겠습니다.
+        const dummyReviews = [
+          { id: 1, rating: 4, comment: '좋아요' },
+          { id: 2, rating: 5, comment: '정말 맛있어요!' },
+          { id: 3, rating: 3, comment: '보통이에요' },
+          { id: 4, rating: 2, comment: '별로에요' },
+          // Add more dummy reviews if needed
+        ];
+
+        setReviews(dummyReviews);
+        const firstVisibleReviews = dummyReviews.slice(0, visibleReviewCount);
+        setVisibleReviews(firstVisibleReviews);
+
+        // Toggle the "Load More" button based on whether more reviews are available
+        setShowLoadMore(visibleReviewCount < dummyReviews.length);
+      } catch (error) {
+        console.error('Error fetching reviews:', error);
+      }
+    };
+
+    fetchReviews();
+  }, [route.params?.itemId, visibleReviewCount]); // Update when the count changes
+
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.homeButton}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Ionicons name="arrow-back" size={24} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.title}>술 상세 정보</Text>
+        <Image source={itemDetail.image} style={styles.image} />
+        {/* 평균 별점을 나타내는 UI */}
+        <View style={styles.averageRatingContainer}>
+          <Text style={styles.averageRatingText}>평균 별점:</Text>
+          <Rating
+            readonly
+            startingValue={calculateAverageRating()}
+            imageSize={20}
+          />
+          <Text style={styles.averageRatingNumber}>
+            ({calculateAverageRating().toFixed(1)})
+          </Text>
+        </View>
+        <Text style={styles.detailText}>
+          <Text style={styles.labelText}>술 이름:</Text> {itemDetail.name}
+        </Text>
+        <Text style={styles.detailText}>
+          <Text style={styles.labelText}>가격:</Text> ₩{itemDetail.price}
+        </Text>
+        <Text style={styles.detailText}>
+          <Text style={styles.labelText}>설명:</Text> {itemDetail.description}
+        </Text>
+        <Card.Divider style={{ marginTop: 20 }} />
+        <View style={styles.noteTitleView}>
+          <Text style={styles.noteTitleText}>별점을 선택해주세요</Text>
+        </View>
+        <Rating
+          startingValue={calculateAverageRating()} // 평균 별점으로 변경
+          onFinishRating={(rating) => setRatingValue(rating)}
+          style={{ paddingVertical: 10 }}
+        />
+        {/* 리뷰 작성 UI */}
+        <Text style={styles.reviewTitle}>리뷰 작성</Text>
+        <TextInput
+          style={styles.reviewInput}
+          placeholder="리뷰를 작성하세요"
+          multiline
+          value={review}
+          onChangeText={(text) => setReview(text)}
+        />
+        <TouchableOpacity
+          style={styles.reviewButton}
+          onPress={handleReviewSubmit}
+        >
+          <Text style={styles.reviewButtonText}>리뷰 작성 완료</Text>
+        </TouchableOpacity>
+        <Text style={styles.reviewListTitle}>리뷰 목록</Text>
+        <FlatList
+    scrollEnabled={false}
+    data={visibleReviews}
+    keyExtractor={(item) => item.id.toString()}
+    renderItem={({ item }) => (
+      <View style={styles.reviewItem}>
+        <Text style={styles.reviewItemText}>
+          별점: {item.rating}, 리뷰: {item.comment}
+        </Text>
+        <TouchableOpacity onPress={() => handleLike(item.id)}>
+          <Ionicons
+            name={'heart'}
+            size={24}
+            color={item.liked ? 'red' : 'black'}
+          />
+        </TouchableOpacity>
+      </View>
+     )}
+      
+        />
+        {showLoadMore ? (
+          <TouchableOpacity onPress={loadMoreReviews}>
+            <Text style={{ color: 'blue', marginTop: 10 }}>더 보기</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={closeReviews}>
+            <Text style={{ color: 'red', marginTop: 10 }}>닫기</Text>
+          </TouchableOpacity>
+        )}
+        <Button
+          title="테이스팅노트 작성"
+          onPress={() => navigation.navigate('WriteDownNote')}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -55,6 +219,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     justifyContent: 'flex-start',
+    backgroundColor: 'white',
   },
   title: {
     fontSize: 24,
@@ -69,12 +234,86 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   detailText: {
-    fontSize: 25,
     marginBottom: 8,
     textAlign: 'left',
   },
   labelText: {
     fontWeight: 'bold',
+  },
+
+  reviewTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  reviewInput: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    height: 100,
+  },
+  reviewButton: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    width: 350,
+    borderRadius: 5,
+    alignItems: 'center',
+    height: 70,
+  },
+  reviewButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+
+  averageRatingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  averageRatingText: {
+    fontSize: 16,
+    marginRight: 5,
+  },
+  averageRatingNumber: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  homeButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    padding: 5,
+    borderRadius: 5,
+    marginTop: 10,
+    width: 30,
+    height: 50,
+  },//좋아요 순위=추천순
+  noteTitleView: {
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  noteTitleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  reviewListTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  reviewItem: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  reviewItemText: {
+    fontSize: 15,
   },
 });
 
--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
-import { StyleSheet, Text, SafeAreaView, ScrollView, TouchableOpacity, Image, View } from 'react-native';
+import { StyleSheet, Text, Card, SafeAreaView, ScrollView, TouchableOpacity, Image, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useNavigation } from '@react-navigation/native';
 import { Tab, TabView } from '@rneui/themed';
@@ -13,26 +13,37 @@ function MenuSeparator() {
 export default function Home() {
   const navigation = useNavigation();
   const [index, setIndex] = React.useState(0);
+  const [additionalMenuIndex, setAdditionalMenuIndex] = React.useState(0);
+  const [additionalMenuItems, setAdditionalMenuItems] = React.useState(['맥주', '리큐르', '와인', '막걸리']);
+
+  const handleMenuItemPress = (itemId) => {
+    navigation.navigate('Detail', { id: itemId });
+  };
+
+
+  const handleAdditionalMenuPress = (menuIndex) => {
+    setAdditionalMenuIndex(menuIndex);
+  };
 
   const getMenuItems = () => {
     switch (index) {
       case 0:
         return [
-          { id: 1, title: '메뉴 1', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 2, title: '메뉴 2', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 3, title: '메뉴 3', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 4, title: '메뉴 4', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 5, title: '메뉴 5', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 6, title: '메뉴 6', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 7, title: '메뉴 7', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 1, title: '데이터 1', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 2, title: '데이터 2', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 3, title: '데이터 3', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 4, title: '데이터 4', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 5, title: '데이터 5', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 6, title: '데이터 6', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 7, title: '데이터 7', image: require('../../images/alcholicons/sd.jpg') },
         ];
       case 1:
         return [
-          { id: 6, title: '메뉴 6', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 7, title: '메뉴 7', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 8, title: '메뉴 8', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 9, title: '메뉴 9', image: require('../../images/alcholicons/sd.jpg') },
-          { id: 10, title: '메뉴 10', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 6, title: '데이터 6', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 7, title: '데이터 7', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 8, title: '데이터 8', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 9, title: '데이터 9', image: require('../../images/alcholicons/sd.jpg') },
+          { id: 10, title: '데이터 10', image: require('../../images/alcholicons/sd.jpg') },
           
         ];
       case 2:
@@ -43,13 +54,12 @@ export default function Home() {
           { id: 14, title: '메뉴 14', image: require('../../images/alcholicons/sd.jpg') },
           { id: 15, title: '메뉴 15', image: require('../../images/alcholicons/sd.jpg') },
         ];
+        
       default:
         return [];
+        
     }
-  };
-
-  const handleMenuItemPress = (itemId) => {
-    navigation.navigate('Detail', { id: itemId, });
+    
   };
 
   return (
@@ -88,27 +98,17 @@ export default function Home() {
   />
 </Tab>
 <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-<View style={styles.additionalMenu}>
-<TouchableOpacity>
-  <Text style={styles.additionalMenuText}>추가 메뉴 1</Text>
-</TouchableOpacity>
-<TouchableOpacity>
-  <Text style={styles.additionalMenuText}>추가 메뉴 2</Text>
-</TouchableOpacity>
-<TouchableOpacity>
-  <Text style={styles.additionalMenuText}>추가 메뉴 2</Text>
-</TouchableOpacity>
-<TouchableOpacity>
-  <Text style={styles.additionalMenuText}>추가 메뉴 2</Text>
-</TouchableOpacity>
-<TouchableOpacity>
-  <Text style={styles.additionalMenuText}>추가 메뉴 2</Text>
-</TouchableOpacity>
-<TouchableOpacity>
-  <Text style={styles.additionalMenuText}>추가 메뉴 2</Text>
-</TouchableOpacity>
-</View>
-</ScrollView>
+        <View style={styles.additionalMenu}>
+          {additionalMenuItems.map((menu, menuIndex) => (
+            <TouchableOpacity
+              key={menuIndex}
+              onPress={() => handleAdditionalMenuPress(menuIndex)}
+            >
+              <Text style={[styles.additionalMenuText, { color: menuIndex === additionalMenuIndex ? 'green' : 'black' }]}>{menu}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
 
         <ScrollView style={styles.scrollView}>
 
